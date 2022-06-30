47b42aefdcb8f068a63d6140ce80e96f70fd29b6
ArbazTechtiz <akhan@techtiz.com>
2022-06-30 17:51:57 +0500

    size issue is resolved

--- SmartContract.sol
+++ SmartContract.sol
@@ -1,20 +1,24 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.4.18;
+pragma solidity >= 0.5.0 < 0.9.0;
 contract Tokenmart{
+    uint public count;
      struct NftInfo{
      string totalItems;
      string url;
      string nftName;
      }
     mapping (uint => NftInfo) AllNfts;
-     function setNftInfo (uint id,string name, string items, string link) public {
+     function setNftInfo (uint id,string memory name, string memory items, string memory link) public {
          AllNfts[id].totalItems = items;
          AllNfts[id].url = link;
          AllNfts[id].nftName = name;
-         
+         count +=1;
      }
-     function getNftInfo (uint id) public view returns (string, string, string){
+     function getNftInfo (uint id) public view returns (string memory, string memory, string memory){
          return (AllNfts[id].totalItems, AllNfts[id].url, AllNfts[id].nftName);
      }
+     function getCollectionSize() public view returns (uint){
+         return count;
+     } 
 
 }
